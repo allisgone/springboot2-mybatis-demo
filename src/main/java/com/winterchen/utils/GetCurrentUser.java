0fd3b564e7bbbc682ef768cffd53c067be1a0902
yij <yij@sqqmall.com>
2021-05-10 10:41:03 +0800

    线上第一版

--- src/main/java/com/winterchen/utils/GetCurrentUser.java
+++ src/main/java/com/winterchen/utils/GetCurrentUser.java
@@ -4,11 +4,18 @@ import com.alibaba.fastjson.JSON;
 import com.winterchen.model.MedCustomerDomain;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 public class GetCurrentUser {
     public static MedCustomerDomain convertFormRequest(HttpServletRequest request){
-        String sessionId = request.getHeader("Med-SessionId");
-        MedCustomerDomain currentUser = JSON.parseObject(sessionId,MedCustomerDomain.class);
-        return currentUser;
+        try {
+            String sessionId = request.getHeader("Med-SessionId");
+            MedCustomerDomain currentUser = JSON.parseObject(URLDecoder.decode(sessionId, "utf-8"), MedCustomerDomain.class);
+            return currentUser;
+        }catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
